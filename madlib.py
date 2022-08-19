711611bc1d03cfca3c14540550bc4bef167abc12
Kieu Nguyen <ktmindi@gmail.com>
2022-08-18 17:20:16 -0700

    creating my own madlib

---
+++ madlib.py
@@ -0,0 +1,19 @@
+
+
+adj = input("Adjective: ")
+name = input("Name: ")
+interjection1 = input("Interjection: ")
+gradelevel = input("School Grade Level: ")
+color = input("Color: ")
+food = input("Food: ")
+adj2 = input("Adjective: ")
+verb1 = input("Verb: ")
+famous_person = input("Famous Person: ")
+adj3 = input("Adjective: ")
+adj4 = input("Adjective: ")
+hobby = input("Hobby: ")
+verb2 = input("Verb: ")
+
+madlib = f"Hi my name is {adj} {name} and today is my first {interjection1} day of {gradelevel} grade! For lunch my mom packed me my favorite, {color} {food} it is so {adj2} my favorite part is immediately having the urge to {verb1} right after eating it. My teacher this year looks like {famous_person} with the same {adj3} eyebrows and {adj4} swag. I really hope we learn about {hobby} this year. I am so excited I could just {verb2}"
+
+print(madlib)
